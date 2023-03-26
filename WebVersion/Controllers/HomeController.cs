cdb9c7ef3842406b891a77b2bf6a3ff1a0d4dd9f
Aylin-ai <ajlinkadan3224@gmail.com>
2023-03-26 17:09:32 +0300

    Добавил переключение вкладок регистрации и входа на главной странице, а также контроллеры к этим формам; в Program.cs добавли строку, заставляющую контроллеры работать; перенес все стили из отдельного файла прямо в файл Index.cshtml в связи с неотображением стилей

--- WebVersion/Controllers/HomeController.cs
+++ WebVersion/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+
 namespace WebVersion.Controllers
 {
     public class HomeController : Controller
@@ -12,6 +13,23 @@ namespace WebVersion.Controllers
         public IActionResult UserProfile()
         {
             return View();
+        }
+
+        public string AboutAnTrap()
+        {
+            return "О Antrap";
+        }
+
+        public IActionResult Registration(string Login, string Password1, string Password2)
+        {
+            string registrationData = $"Login: {Login}, Password1: {Password1}, Password2: {Password2}";
+            return Content(registrationData);
+        }
+
+        public IActionResult Authorization(string Login, string Password1)
+        {
+            string registrationData = $"Login: {Login}, Password: {Password1}";
+            return Content(registrationData);
         }
     }
 }
