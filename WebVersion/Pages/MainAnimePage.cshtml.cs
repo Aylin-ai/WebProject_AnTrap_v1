a5e7ab560d83e00b4e1c0485b7c2570a87bdaf24
Aylin-ai <ajlinkadan3224@gmail.com>
2023-03-28 16:46:10 +0300

    Реализована функция перелистывания страниц с аниме

--- WebVersion/Pages/MainAnimePage.cshtml.cs
+++ WebVersion/Pages/MainAnimePage.cshtml.cs
@@ -6,23 +6,31 @@ using ShikimoriSharp;
 using System.Net.Http;
 using WebVersion.Models;
 using ShikimoriSharp.Classes;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WebVersion.Pages
 {
     public class MainAnimePageModel : PageModel
     {
+        public int Id { get; set; } = 1;
         private IHttpClientFactory _httpClientFactory;
         private ILogger logger;
         public List<Anime> AnimeList { get; set; } = new List<Anime>();
 
+        public List<SelectListItem> PagesId { get; set; } = new List<SelectListItem>();
+
         public MainAnimePageModel(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
+            for (int i = 1; i <= 406; i++)
+            {
+                PagesId.Add(new SelectListItem { Value = i.ToString(), Text = i.ToString() });
+            }
         }
 
         public async Task OnGetAsync()
         {
-            await GetAnimes(23);
+            await GetAnimes(1);
         }
 
         public async Task GetAnimes(int page)
@@ -37,5 +45,15 @@ namespace WebVersion.Pages
             AnimeList = search.ToList();
         }
 
+        public async Task<IActionResult> OnGetAnimesById(int id)
+        {
+            Id = id;
+            await GetAnimes(Id);
+            if (AnimeList == null)
+            {
+                return Content("Ошибка");
+            }
+            return Page();
+        }
     }
 }
